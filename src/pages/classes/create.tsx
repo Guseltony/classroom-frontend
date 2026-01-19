f23422fbba14dce89e9b7d7f1af498d1f12b9c98
Akanji Anthony Guseltony <devguselt@gmail.com>
2026-01-19 23:48:09 +0100

    Restore project after git directory corruption

---
+++ src/pages/classes/create.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ClassesCreate = () => {
+  return <div>ClassesCreate</div>;
+};
+
+export default ClassesCreate;
---
+++ src/providers/data.ts
@@ -0,0 +1,102 @@
+import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
+
+import { ListResponse } from "@/types";
+import { BACKEND_BASE_URL } from "@/constants";
+
+// CreateResponse, GetOneResponse
+
+if (!BACKEND_BASE_URL) {
+  throw new Error(
+    "BACKEND_BASE_URL is not configured. Please set VITE_BACKEND_BASE_URL in your .env file.",
+  );
+}
+
+const options: CreateDataProviderOptions = {
+  getList: {
+    getEndpoint: ({ resource }) => resource,
+
+    buildQueryParams: async ({ resource, pagination, filters }) => {
+      const page = pagination?.currentPage ?? 1;
+      const pageSize = pagination?.pageSize ?? 10;
+      const params: Record<string, string | number> = { page, limit: pageSize };
+
+      // if (pagination?.mode !== "off") {
+      //   const page = pagination?.currentPage ?? 1;
+      //   const pageSize = pagination?.pageSize ?? 10;
+
+      //   params.page = page;
+      //   params.limit = pageSize;
+      // }
+
+      filters?.forEach((filter) => {
+        const field = "field" in filter ? filter.field : "";
+        const value = String(filter.value);
+
+        // if (field === "role") {
+        //   params.role = value;
+        // }
+
+        // if (resource === "departments") {
+        //   if (field === "name" || field === "code") params.search = value;
+        // }
+
+        // if (resource === "users") {
+        //   if (field === "search" || field === "name" || field === "email") {
+        //     params.search = value;
+        //   }
+        // }
+
+        if (resource === "subjects") {
+          if (field === "department") params.department = value;
+          if (field === "name" || field === "code") params.search = value;
+        }
+
+        // if (resource === "classes") {
+        //   if (field === "name") params.search = value;
+        //   if (field === "subject") params.subject = value;
+        //   if (field === "teacher") params.teacher = value;
+        // }
+      });
+
+      return params;
+    },
+
+    mapResponse: async (response) => {
+      const payload: ListResponse = await response.clone().json();
+
+      console.log("map:", payload);
+      return payload.data ?? [];
+    },
+
+    getTotalCount: async (response) => {
+      const payload: ListResponse = await response.json();
+
+      console.log("totalCount:", payload);
+      return payload.pagination?.total ?? payload.data?.length ?? 0;
+    },
+  },
+
+  // create: {
+  //   getEndpoint: ({ resource }) => resource,
+
+  //   buildBodyParams: async ({ variables }) => variables,
+
+  //   mapResponse: async (response) => {
+  //     const json: CreateResponse = await response.json();
+  //     return json.data ?? {};
+  //   },
+  // },
+
+  // getOne: {
+  //   getEndpoint: ({ resource, id }) => `${resource}/${id}`,
+
+  //   mapResponse: async (response) => {
+  //     const json: GetOneResponse = await response.json();
+  //     return json.data ?? {};
+  //   },
+  // },
+};
+
+const { dataProvider } = createDataProvider(BACKEND_BASE_URL, options);
+
+export { dataProvider };
